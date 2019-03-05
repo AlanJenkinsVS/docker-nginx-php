8f69f5dde187456272d6c7ded69da3902d4bd439
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2019-03-05 09:57:20 +0000

    Initial commit

---
+++ code/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Nginx and PHP7-FPM</title>
+</head>
+<body>
+  <h1>Nginx and PHP7-FPM</h1>
+
+  <?php echo phpinfo(); ?>
+</body>
+</html>
